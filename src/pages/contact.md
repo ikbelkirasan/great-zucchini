a3d1f471ad8b69b9f7db0c7c16fc4916d9492409
Stackbit <projects@stackbit.com>
2019-12-16 03:05:12 +0000

    initial commit by Stackbit

---
+++ src/pages/contact.md
@@ -0,0 +1,17 @@
+---
+title: Contact
+sections:
+  - section_id: contact
+    type: section_contact
+    bg: gray
+    title: Contact
+    content: >-
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam a metus
+      quis lorem malesuada luctus. Cras lacinia, eros at dapibus molestie, risus
+      tortor pretium ligula.
+menus:
+  secondary:
+    weight: 3
+    title: Contact
+template: landing
+---
---
+++ stackbit-build.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+set -e
+set -o pipefail
+set -v
+
+curl -s -X POST https://api.stackbit.com/project/5df6f3e89a07490012a611db/webhook/build/pull > /dev/null
+if [[ -z "${STACKBIT_API_KEY}" ]]; then
+    echo "WARNING: No STACKBIT_API_KEY environment variable set, skipping stackbit-pull"
+else
+    npx @stackbit/stackbit-pull --stackbit-pull-api-url=https://api.stackbit.com/pull/5df6f3e89a07490012a611db 
+fi
+curl -s -X POST https://api.stackbit.com/project/5df6f3e89a07490012a611db/webhook/build/ssgbuild > /dev/null
+gatsby build
+curl -s -X POST https://api.stackbit.com/project/5df6f3e89a07490012a611db/webhook/build/publish > /dev/null
